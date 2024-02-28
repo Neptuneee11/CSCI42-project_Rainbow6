3d38460f94ab879eb6f70186a4aa5198e2b7ba08
Andrew Idquival <andrew.idquival@obf.ateneo.edu>
2024-02-28 11:24:42 +0800

    Revert "Created Customer table in sql file"
    This reverts commit ca8fb04e66a6f1fc01817462a044b5b2748c805d.

--- database/mysqldatabase.sql
+++ database/mysqldatabase.sql
@@ -2,12 +2,3 @@ CREATE DATABASE my_test_db;
 CREATE USER 'test'@'localhost' IDENTIFIED BY 'Secret_1234';
 GRANT ALL PRIVILEGES ON `my_test_db` . * TO 'test'@'localhost';
 FLUSH PRIVILEGES; 
-USE my_test_db;
-
-
-CREATE TABLE Customer (
-	customer_id INT NOT NULL UNIQUE PRIMARY KEY,
-    first_name VARCHAR(15),
-    last_name VARCHAR(15)
-);
-
