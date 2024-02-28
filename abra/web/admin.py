669261f76c550a480e030620200f85bf3dedf61e
jmAmador <125117981+jmAmador@users.noreply.github.com>
2024-02-28 20:00:35 +0800

    admin panel changes
    can now see tables and add/ change data

--- abra/web/admin.py
+++ abra/web/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Customer, Authentication, Bike, Transaction
 
 # Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+class AuthenticationAdmin(admin.ModelAdmin):
+    pass
+
+class BikeAdmin(admin.ModelAdmin):
+    pass
+
+class TransactionAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Authentication, AuthenticationAdmin)
+admin.site.register(Bike, BikeAdmin)
+admin.site.register(Transaction, TransactionAdmin)
