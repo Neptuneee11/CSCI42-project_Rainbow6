378e020d189b1e665ad6b9c49eb4f102e23d8c8e
Andrew Idquival <andrew.idquival@obf.ateneo.edu>
2024-02-28 13:31:52 +0800

    Created Customer table in sql file

--- database/mysqldatabase.sql
+++ database/mysqldatabase.sql
@@ -1,4 +1,10 @@
 CREATE DATABASE my_test_db;
 CREATE USER 'test'@'localhost' IDENTIFIED BY 'Secret_1234';
 GRANT ALL PRIVILEGES ON `my_test_db` . * TO 'test'@'localhost';
-FLUSH PRIVILEGES; 
+FLUSH PRIVILEGES;
+
+CREATE TABLE Customer (
+    customer_id int NOT NULL UNIQUE PRIMARY KEY,
+    first_name VARCHAR(20),
+    last_name VARCHAR(20)
+);
