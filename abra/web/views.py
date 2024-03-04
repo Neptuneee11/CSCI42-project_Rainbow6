17f00999031d1e81193df9bace68a8774c05cbf3
Andrew Idquival <andrew.idquival@obf.ateneo.edu>
2024-03-04 20:15:21 +0800

    Update index.html to add latitude and longitude

--- abra/web/views.py
+++ abra/web/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import requests
-import json
 
 def index(request):
-    ip = requests.get('https://api.ipify.org?format=json')
-    ip_data = json.loads(ip.text)
-    res = requests.get('http://ip-api.com/json/' + ip_data["ip"])
-    location_data_one = res.text
-    location_data = json.loads(location_data_one)
-    return render(request, 'index.html', {'data': location_data})
+    return render(request, 'index.html')
