dc84c2cc64d44f3675b549a5434589478b73737e
Andrew Idquival <andrew.idquival@obf.ateneo.edu>
2024-03-04 16:13:37 +0800

    Connected location data with index.html

--- abra/web/views.py
+++ abra/web/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+import json
 
 def index(request):
-    return render(request, 'index.html')
+    res = requests.get('http://ip-api.com/json/24.48.0.1')
+    location_data_one = res.text
+    location_data = json.loads(location_data_one)
+    return render(request, 'index.html', {'data': location_data})
