ce5c48f5b316444597126bed8f08178a78545e89
NeptuneReal <38521285+Neptuneee11@users.noreply.github.com>
2024-03-05 00:29:06 +0800

    Models.py made to accommodate qr codes for each bike

--- venv/abra/web/models.py
+++ venv/abra/web/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+import qrcode
+from io import BytesIO
+from django.core.files import File
+from PIL import Image, ImageDraw
 
 class Customer(models.Model):
     Customer_ID = models.IntegerField(primary_key=True)
@@ -10,9 +14,26 @@ class Authentication(models.Model):
     Customer_ID = models.ForeignKey(Customer, on_delete=models.CASCADE)
     Password = models.CharField(max_length=255, primary_key=True)
 
+
 class Bike(models.Model):
+    Code = models.ImageField(blank=True, upload_to='Code')
     Bike_NO = models.CharField(max_length=255, primary_key=True)
 
+    def __str__(self) -> str:
+        return self.Bike_NO
+    
+    def save(self, *args, **kwargs):
+        qr_image = qrcode.make(self.Bike_NO)
+        qr_offset = Image.new('RGB', (310,310), 'white')
+        qr_offset.paste(qr_image)
+        files_name = f'{self.Bike_NO}-{self.id}qr.png'
+        stream = BytesIO()
+        qr_offset.save(stream, 'PNG')
+        self.Code.save(files_name, File(stream), save=False)
+        qr_offset.close()
+        super().save(*args,**kwargs)
+
+    
 class Transaction(models.Model):
     Transaction_NO = models.CharField(max_length=255, primary_key=True)
     Customer_ID = models.ForeignKey(Customer, on_delete=models.CASCADE)
