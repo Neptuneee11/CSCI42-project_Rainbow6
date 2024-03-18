6bab2b77087e7d4379c7aeeca12ab627844767e0
jmAmador <125117981+jmAmador@users.noreply.github.com>
2024-03-18 12:56:37 +0800

    created the homepage and the login page

--- venv/abra/abra/urls.py
+++ venv/abra/abra/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('web/', include('web.urls', namespace='web')),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
