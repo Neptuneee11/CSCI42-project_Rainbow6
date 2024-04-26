44b2e6230032ed73befbf36f550287b8d420061c
Joaquin Hermosisima <007heinekenlover@gmail.com>
2024-04-26 18:39:34 +0800

    Set up views.py

--- venv/abra/Customer/views.py
+++ venv/abra/Customer/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import CustomUser
 
-# Create your views here.
-from django.http import HttpResponse
+class CustomUserCreationForm(UserCreationForm):
+    full_name = forms.CharField(max_length=100)
+    id_number = forms.CharField(max_length=20)
+    school_year = forms.CharField(max_length=4)
+    course = forms.CharField(max_length=100)
+    phone_number = forms.CharField(max_length=15)
 
-def index(request):
-    return HttpResponse('This is from the Customer index')
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'password1', 'password2', 'full_name', 'id_number', 'school_year', 'course', 'phone_number', 'email')
+
+class CustomUserAuthenticationForm(AuthenticationForm):
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'password')
